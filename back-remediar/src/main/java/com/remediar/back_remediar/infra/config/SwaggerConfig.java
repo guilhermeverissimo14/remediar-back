2e47d3bd90ed006bfc263595333d78ff72419819
guilhermeverissimo14 <guilhermeverissimo14@gmail.com>
2025-12-03 12:35:38 -0300

    Configurar Swagger para usar HTTPS em produção

--- back-remediar/src/main/java/com/remediar/back_remediar/infra/config/SwaggerConfig.java
+++ back-remediar/src/main/java/com/remediar/back_remediar/infra/config/SwaggerConfig.java
@@ -2,11 +2,18 @@ package com.remediar.back_remediar.infra.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${server.servlet.context-path:}")
+    private String contextPath;
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -14,6 +21,14 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("API - Remediar")
                         .version("1.0")
-                        .description("Documentação da API da ONG Remediar"));
+                        .description("Documentação da API da ONG Remediar"))
+                .servers(List.of(
+                        new Server()
+                                .url("https://remediar-api.mgioqc.easypanel.host" + contextPath)
+                                .description("Servidor de Produção"),
+                        new Server()
+                                .url("http://localhost:8081" + contextPath)
+                                .description("Servidor Local")
+                ));
     }
 }
