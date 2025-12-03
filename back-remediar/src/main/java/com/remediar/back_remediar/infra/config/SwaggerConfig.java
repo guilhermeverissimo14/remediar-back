ff67762163f6a539681e8576ece2b873f9e5fd37
guilhermeverissimo14 <guilhermeverissimo14@gmail.com>
2025-12-03 15:14:22 -0300

    fix: desfazendo alteracoes

--- back-remediar/src/main/java/com/remediar/back_remediar/infra/config/SwaggerConfig.java
+++ back-remediar/src/main/java/com/remediar/back_remediar/infra/config/SwaggerConfig.java
@@ -2,18 +2,11 @@ package com.remediar.back_remediar.infra.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
 public class SwaggerConfig {
-
-    @Value("${server.servlet.context-path:}")
-    private String contextPath;
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -21,14 +14,6 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("API - Remediar")
                         .version("1.0")
-                        .description("Documentação da API da ONG Remediar"))
-                .servers(List.of(
-                        new Server()
-                                .url("https://remediar-api.mgioqc.easypanel.host" + contextPath)
-                                .description("Servidor de Produção"),
-                        new Server()
-                                .url("http://localhost:8081" + contextPath)
-                                .description("Servidor Local")
-                ));
+                        .description("Documentação da API da ONG Remediar"));
     }
 }
